8723c0d3cfec406b22c2cb98c7ba2453ac801b4f
Andreys-Gp <109758319+Andreys-Gp@users.noreply.github.com>
2023-09-11 18:16:16 -0500

    reload

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -3,18 +3,19 @@ import "./Footer.css"
 const Footer = () => {
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.facebook.com/brian.gp.96/'>
                 <img src="/img/facebook.png" alt='Facebook' />
             </a>
-            <a href='https://www.aluracursos.com/'>
-                <img src="/img/twitter.png" alt='twitter' />
+            <a href='https://github.com/Andreys-Gp'>
+                <img src="/img/mark-github.svg" alt='gitHub' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/brayanandreysgonzalez/'>
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
+            
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado Andreys Gp</strong>
     </footer>
 }
 
